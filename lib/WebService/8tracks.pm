70ea8d74fe0b77fb2c36acc1fea1a787f5b1332f
motemen <motemen@gmail.com>
2010-05-21 13:29:59 +0900

    initial commit

---
+++ lib/WebService/8tracks.pm
@@ -0,0 +1,33 @@
+package WebService::8tracks;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::8tracks -
+
+=head1 SYNOPSIS
+
+  use WebService::8tracks;
+
+=head1 DESCRIPTION
+
+WebService::8tracks is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
